ca4d15ae0308ea5790db9dc9045b7b041494ef15
martingerardoc <u238513729@byui.edu>
2026-01-10 04:52:11 -0300

    first commit

--- server.js
+++ server.js
@@ -6,6 +6,7 @@
  * Require Statements
  *************************/
 const express = require("express")
+const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
@@ -14,6 +15,12 @@ const static = require("./routes/static")
  * Routes
  *************************/
 app.use(static)
+
+/* ***********************
+ * Routes
+ *************************/
+app.use(static)
+
 
 /* ***********************
  * Local Server Information
